e4954f96a88d033a011a9dd7dc2145fe3713d882
dipa.alhaza <dipa.alhaza@ui.ac.id>
2024-05-06 14:19:39 +0700

    fix: fix bug

--- src/main/java/com/unimate/unimate/service/PartnershipService.java
+++ src/main/java/com/unimate/unimate/service/PartnershipService.java
@@ -3,7 +3,10 @@ package com.unimate.unimate.service;
 import com.unimate.unimate.dto.PartnershipDTO;
 import com.unimate.unimate.dto.UpdatePartnershipDTO;
 import com.unimate.unimate.entity.Partnership;
+
+
 import java.util.List;
+import java.util.Map;
 
 public interface PartnershipService {
     void savePartnership(Partnership partnership);
@@ -19,4 +22,16 @@ public interface PartnershipService {
     Partnership createPartnership(PartnershipDTO partnershipDTO);
 
     Partnership updatePartnership(UpdatePartnershipDTO updatePartnershipDTO);
+
+    public List<Object[]> getMonthlyRequests(int year);
+
+    public List<Map<String, Integer>> getDailyRequests(int year, int month);
+
+
+    public Map<Integer, Integer> getMonthlyRequestsForAllYears();
+
+
+    public List<Map<String, Object>> getRequestsByStatus(Integer year, Integer month);
+
+
 }
